cad8b848cdca3d456d86f0dc34c6393d1fbea142
Qing <chingwang.qw@gmail.com>
2019-11-02 16:15:37 +0000

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,27 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :owner, :mechanic
 
-  def initialize(make, model, classification)
+  @@all = []
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.car_classifications
+    self.all.map {|c| c.classification}
+  end
+
+  #Get a list of mechanics that have an expertise that matches the car classification
+  def self.list_mechanic(classification)
+    Mechanic.all.select {|m| m.specialty == classification}
+  end
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,30 @@
 class Mechanic
 
   attr_reader :name, :specialty
+  @@all = []
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def self.all
+    @@all
+  end
 
+  #Get a list of all cars that a mechanic services
+  def cars
+    Car.all.select {|c| c.mechanic == self}
+  end
+
+  #Get a list of all the car owners that go to a specific mechanic
+  def car_owners
+    cars.map {|c| c.owner}
+  end
+
+  #Get a list of the names of all car owners who go to a specific mechanic
+  def list_owner_names
+    car_owners.map {|co| co.name}.uniq
+  end
 
 end
